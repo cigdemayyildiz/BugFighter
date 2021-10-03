e6f32eea6980c93ca6799c98c5046fe03954e254
Cigdem <cigdemrespect@gmail.com>
2021-10-03 15:48:54 -0500

    Operators

--- src/BugFighter/A05Operators.java
+++ src/BugFighter/A05Operators.java
@@ -1,4 +1,52 @@
 package BugFighter;
 
 public class A05Operators {
+
+    public static void main(String[] args) {
+
+           /*
+            - Aritmetik Operatorler (Arithmetic Operators)
+                - Toplama (Addition) --> (a + b)
+                - Cikarma (Subtraction) --> (a - b)
+                - Carpma (Multiplication) --> (a * b)
+                - Bolme (Division) --> (a / b)
+                - Mod Alma (Modulus) --> (a % b)
+                - 1 artirma (Increment) --> (a++)
+                - 1 eksiltme (Decrement) --> (a--)
+
+            - Iliskisel Operatorler (Relational Operators)
+                - Esitlik (equal to) --> (a == b)
+                - Esit Degil (not equal to) --> (a != b)
+                - Buyuktur (greater than) --> (a > b)
+                - Kucuktur (less than) --> (a < b)
+                - Buyuk Esittir (greater than or equal to) --> (a >= b)
+                - Kucuk Esittir (less than or equal to) --> (a <= b)
+
+            - Mantiksal Operatorler (Logical Operators)
+                - Ve (logical and) --> (a && b)
+                - Veya (logical or) --> (a || b)
+                - Degil (logical not) --> !(a && b)
+
+            - Kosul Operatoru (Ternary Operator)
+            variable x = (expression) ? value if true : value if false
+                - a = 5
+                - b = (a == 1) ? 1 : 0
+                - Ciktisi 0
+
+            - Atama Operatorleri (Assignment Operators)
+                - =     --> Basit atama operatoru. Operatorun sagindaki degiskenin degeri solundaki(lere)ne atanir.
+                        (c = a + b) --> a ve b nin degerini c ye atayacaktir.
+                - +=    --> Operatorun sagindaki degeri solundaki degerle toplayip tekrar solundaki degere atar.
+                        (c += a) --> c, c ile a nin toplamina esittir.
+                - -=    --> Operatorun sagindaki degeri solundaki degerden cikartip tekrar solundaki degere atar.
+                        (c -= a) --> c, a nin c den cikartilmasina esittir.
+                - *=    --> Operatorun sagindaki degeri solundaki deger ile carpip tekrar solundaki degere atar.
+                        (c *= a) --> c, c ile a nin carpimina esittir.
+                - /=    --> Operatorun sagindaki degeri solundaki degere bolup tekrar solundaki degere atar.
+                        (c /= a) --> c, c nin a ya bolumune esittir.
+                - %=    --> Operatorun sagindaki degerin solundakine gore modunu alip tekrar solundaki degere atar.
+                        (c %= a) --> c, c nin a moduna esittir.
+         */
+
+    }
 }
