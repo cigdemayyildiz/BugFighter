391347aaeb6aab44cdd98b92c43767ecb7167478
Cigdem <cigdemrespect@gmail.com>
2021-10-03 15:46:53 -0500

    Variables

--- src/BugFighter/A02Variables.java
+++ src/BugFighter/A02Variables.java
@@ -1,4 +1,41 @@
 package BugFighter;
 
 public class A02Variables {
+
+    public static void main(String[] args) {
+
+        /*
+        Variable (Degiskenler)
+        Degiskenler bilgisayar programlamada gecici bilgilerin tutuldugu ve iceriginin yazilimci tarafindan
+        atandigi bilgilerdir.
+        Degiskenlerin 4 ozelligi bulunur;  Tip, Isim, Deger, Adres
+
+        --> int a = 25;
+
+        Bu komutta a isminde bir degisken int turunde tanimlanmis ve icerisine 25 degeri atanmistir.
+        Bu degiskenin hafizada nerede tutuldugu ise isletim sistemi tarafindan belirlenir. Bu komutta belirttigimiz
+        uzere, bundan sonra programimizda kullanacagimiz tum a degeri 25 olarak algilanacaktir.
+
+        veriTipi   degiskenIsmi  =  degiskenDegeri;
+         */
+
+        /*
+        Veri Tipleri (Data Types)
+
+            - Tam Sayilar
+                byte (8 bit)
+                short (16 bit)
+                int (32 bit)
+                long (64 bit)
+            - Ondalikli Sayilar
+                float (32 bit)  --> float degerinde bir deger girmek istedigimizde tanimlamadan
+                                    sonra f/F koyarak double dan ayirmak gerekir
+                double (64 bit)
+            - Karakterler
+                char (16 bit)
+            - Mantiksal Degerler
+                boolean (true/false)
+         */
+
+    }
 }
