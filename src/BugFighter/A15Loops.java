abb5d2c96ebdc9377e9e0380d3466b761946324a
Cigdem <cigdemrespect@gmail.com>
2021-10-04 09:41:03 -0500

    Loops

--- src/BugFighter/A15Loops.java
+++ src/BugFighter/A15Loops.java
@@ -5,11 +5,23 @@ public class A15Loops {
     public static void main(String[] args) {
 
         /*
-        Donguler, belirli bir kosul saglandigi surece tekrarlanmasi gereken isler icin kullanilan programlama kodlaridir.
-        Java'da FOR, WHILE ve DO-WHILE olmak uzere 3 adet dongu vardir. Donguler sayesinde kodlari dimaniklestiriyoruz.
+        Donguler, belirli bir kosul saglandigi surece tekrarlanmasi gereken isler icin kullanilan
+        programlama kodlaridir.
+
+        Donguler sayesinde kodlari dimaniklestiriyoruz.
+
+        Java'da FOR, WHILE ve DO-WHILE olmak uzere 3 adet dongu vardir.
+
+        Genellikle kac kere donecegi belli olmayan kosullarda While ya da Do-While donguleri kullanilir.
+
+        Elimizde kac kere donecegi belli olan elemanlari olan bir dongu varsa For loop kullanilir.
+
+        Ama su durumda su kullanilmali bu durumda bu kullanilmali diye kesin bir kural yoktur.
+
+        Tum donguler eger dogru kodlanirsa ayni isi yaparlar.
          */
 
-        // WHILE ve DO-WHILE DONGUSU
+
 
 
 
