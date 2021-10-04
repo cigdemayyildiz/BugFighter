9a277bd31172931c526147a40dfdabd4a52e67e4
Cigdem <cigdemrespect@gmail.com>
2021-10-03 20:15:08 -0500

    Practice

---
+++ src/BugFighter/A15Loops.java
@@ -0,0 +1,17 @@
+package BugFighter;
+
+public class A15Loops {
+
+    public static void main(String[] args) {
+
+        /*
+        Donguler, belirli bir kosul saglandigi surece tekrarlanmasi gereken isler icin kullanilan programlama kodlaridir.
+        Java'da FOR, WHILE ve DO-WHILE olmak uzere 3 adet dongu vardir. Donguler sayesinde kodlari dimaniklestiriyoruz.
+         */
+
+        // WHILE ve DO-WHILE DONGUSU
+
+
+
+    }
+}
