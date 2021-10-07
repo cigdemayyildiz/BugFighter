d11c9f82227b5f0741dba30beae42ee70b55a1d2
Cigdem <cigdemrespect@gmail.com>
2021-10-07 08:41:58 -0500

    Final

---
+++ src/BugFighter/A35Final.java
@@ -0,0 +1,14 @@
+package BugFighter;
+
+public class A35Final {
+
+    // Final keyword, eger bir Class icinde tanimlanan bir degiskenin degistirilmesini istemiyorsak, o degiskeni
+    // sabitlemek istiyorsak kullanilir.
+
+    // Eger tanimlanan bir degiskenin degistirilmesini istemiyorsak normalde o veriyi private da yapabiliriz ama
+    // degiskeni private yapmak sadece o sinifta o degiskene ulasma sansi verdigi icin bu gibi durumlarda final keyword
+    // u ile degiskeni sabitlemeyi kullaniyoruz.
+
+
+    // KULLANIM ICIN A26Constructor Class'a gidin -->
+}
