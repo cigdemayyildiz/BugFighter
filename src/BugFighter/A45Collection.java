19567759f1880152c0ddccb1f983e3afb33150a7
Cigdem <cigdemrespect@gmail.com>
2021-10-08 16:37:50 -0500

    Collection

--- src/BugFighter/A45Collection.java
+++ src/BugFighter/A45Collection.java
@@ -1,28 +1,80 @@
 package BugFighter;
 
+import java.util.Arrays;
+
 public class A45Collection {
 
     public static void main(String[] args) {
 
         // Collections = Kolleksiyonlar
         // Element adi verilen objectleri gruplar halinde temsil eden bir yapidir.
-        // Bir collection icinde birden fazla degisik data tipinde element tutabiliyoruz.
+        // Bir collection icinde birden fazla fazla object i element olarak tutabiliyoruz.
 
         /*
+
+                                         Iterable
+                                            |
                                         Collection
-
-        Set                             List                              Map
-        HashSet<E>                      ArrayList                         HashMap
-        LinkedHashSet<E>                LinkedList                        LinkedHashMap
-        TreeSet<E>                                                        TreeMap
-
-         */
-
+         --------------------------------------------------------------------
+         |                                  |                               |
+        Set                             List                              Queue
+        HashSet<E>                      ArrayList<E>                      PriorityQueue<E>
+        LinkedHashSet<E>                LinkedList<E>                     LinkedList<E>
+        TreeSet<E>                      Vector<E>
 
 
+        Set
+          HashSet<E>
+            - No order
+            - Stores null value
+            - There is no get(); method to reach out for specific element
+            - Unique element
+
+            LinkedHashSet<E>
+            - Insertion order
+            - Stores null value
+            - There is no get(); method to reach out for specific element
+            - Unique element
+
+             TreeSet<E>
+            - Ascending order
+            - No null value
+            - There is no get(); method to reach out for specific element
+            - Unique element
 
 
 
+        List
+            ArrayList<E>
+            - You can store duplicates
+            - Insertion order
+            - Has indexing --> istenen index e veri eklenebiliyor
+            - Stores objects
+            - Size is dynamic --> baslangicta bir boyut vermemize gerek kalmiyor, sonuza kadar uzayabilir
+
+            LinkedList<E>
+            - You can store duplicates
+            - Insertion order
+            - Has indexing --> istenen index e veri eklenebiliyor
+            - Stores objects
+            - Size is dynamic --> baslangicta bir boyut vermemize gerek kalmiyor, sonuza kadar uzayabilir
+
+            Vector<E>
+            - You can store duplicates
+            - Insertion order
+            - Has indexing --> istenen index e veri eklenebiliyor
+            - Stores objects
+            - Size is dynamic --> baslangicta bir boyut vermemize gerek kalmiyor, sonuza kadar uzayabilir
+
+
+        Queue
+            PriorityQueue<E>
+            -
+
+            LinkedList<E>
+            -
+
+*/
 
 
     }
