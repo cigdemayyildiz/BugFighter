f7550f1da434ad3d40fdddfc7b34c128fb2161f8
Cigdem <cigdemrespect@gmail.com>
2021-10-09 19:06:59 -0500

    MAP

---
+++ src/BugFighter/A52MAP.java
@@ -0,0 +1,20 @@
+package BugFighter;
+
+import java.util.*;
+
+public class A52MAP {
+
+    public static void main(String[] args) {
+
+
+        // MAP
+
+        // Iki tane ayni degerde Key olamaz, Key unique olmak zorunda
+
+        // Map de veri eklemek icin add(); methodu yerine put(); methodu kullaniliyor.
+
+        // Collection larla calisma yapilari birbirine cok benzer
+
+
+    }
+}
