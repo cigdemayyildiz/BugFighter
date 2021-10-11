3194d502ad346e6261be4e0aefc2ff3cf97f85c2
Cigdem <cigdemrespect@gmail.com>
2021-10-11 14:23:54 -0500

    Exception Handling

--- src/BugFighter/A42ExceptionHandling.java
+++ src/BugFighter/A42ExceptionHandling.java
@@ -1,4 +1,11 @@
 package BugFighter;
 
 public class A42ExceptionHandling {
+
+    // Exception Handling
+    // Try-Catch-Finally
+
+    // Exception ile hatalari ayiklayip, Try-Catch-Finally yontemi ile kontrolu saglanir.
+
+
 }
