ef1a80989f010af8cf918ae4ce7918217605f15d
Cigdem <cigdemrespect@gmail.com>
2021-10-12 18:19:04 -0500

    Final

--- src/BugFighter/A35Final.java
+++ src/BugFighter/A35Final.java
@@ -10,4 +10,19 @@ public class A35Final {
     // u ile degiskeni sabitlemeyi kullaniyoruz.
 
     // KULLANIM ICIN A26Constructor Class'a gidin -->
+
+
+
+    /*
+    final int a = 5;
+    a = 6; --> you cannot change if you make it final
+    */
+
+    /*
+    static int b = 4;
+    b = 5; --> you cannot change if you make it static
+     */
+
+
+
 }
