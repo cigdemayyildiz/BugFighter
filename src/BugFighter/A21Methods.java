644a7dd5f33556e58f8709cb3341f5b759a742d6
Cigdem <cigdemrespect@gmail.com>
2021-12-04 21:49:46 -0600

    Methods

--- src/BugFighter/A21Methods.java
+++ src/BugFighter/A21Methods.java
@@ -5,7 +5,6 @@ public class A21Methods {
     public static void main(String[] args) {
 
         // Methods = Functions
-
         // f(x) = (x + 2) * 6
 
         // f methodunun main method icine cagrilmasi
