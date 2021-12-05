4f32b5155b4298b6da1894082c8dd25b551179ea
Cigdem <cigdemrespect@gmail.com>
2021-12-04 21:52:26 -0600

    For Each Loop

--- src/BugFighter/A29ForEach.java
+++ src/BugFighter/A29ForEach.java
@@ -19,7 +19,6 @@ public class A29ForEach {
             System.out.println(lst);
         }
 
-
         // for loop kullanilarak
         System.out.println("For Loop kullanilarak: ");
         for (int i = 0; i < list.length; i++) {
