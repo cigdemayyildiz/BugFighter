179ea260b1f738cf4fc6882c0fbe873155f4bd5a
Cigdem <cigdemrespect@gmail.com>
2022-01-11 16:17:22 -0600

    Encapsulation

--- src/BugFighter/A36Encapsulation.java
+++ src/BugFighter/A36Encapsulation.java
@@ -11,7 +11,6 @@ public class A36Encapsulation {
     // Abstraction = Soyutlama
 
 
-
     // Book sinifimizin oldugunu varsayalim
     public String name, author, publisher;
     private int numberOfPage; // numberOfPage degiskenini sarmaladik, birincisi degiskeni private yaparak koruma altina
