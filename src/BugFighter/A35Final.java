ee0207f3cec01fe76ae1e2bfcb90a384f11cd7a6
Cigdem <cigdemrespect@gmail.com>
2022-01-22 08:18:02 -0600

    Final

--- src/BugFighter/A35Final.java
+++ src/BugFighter/A35Final.java
@@ -6,7 +6,6 @@ public class A35Final {
     // sabitlemek istiyorsak kullanilir.
 
 
-
     // Eger tanimlanan bir degiskenin degistirilmesini istemiyorsak normalde o veriyi private da yapabiliriz ama
     // degiskeni private yapmak sadece o sinifta o degiskene ulasma sansi verdigi icin bu gibi durumlarda final keyword
     // u ile degiskeni sabitlemeyi kullaniyoruz.
